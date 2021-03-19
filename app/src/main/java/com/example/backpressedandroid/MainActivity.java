efb4b0c09efc6b8a59c689121e25a012bf2f82c3
34747 <farawikhan@gmail.com>
2021-03-20 01:16:09 +0600

    practise problem

---
+++ app/src/main/java/com/example/backpressedandroid/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.backpressedandroid;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.WebViewId);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.getJavaScriptEnabled();
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.youtube.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
